173394410bc3c8134c722903e7dd127f7987ac8a
yeasinshaikh188@gmail.com <yeasinshaikh188@gmail.com>
2024-09-12 20:47:27 +0600

    finished the nav bar section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily :{
+        poppins: ["Poppins", "sans-seri"]
+      },
+      colors: {
+        "bg-color": "#091420",
+        "bg-dark-color": "#0c1a28",
+        "primary-color": "#4fe98c",
+      },
+      width: {
+        'custom-image-width' : '200px'
+      }
+    },
   },
   plugins: [],
-}
-
+};
