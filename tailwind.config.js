c20b0830f166df5cea97313ffb7c929f9ce8d084
yeasinshaikh188@gmail.com <yeasinshaikh188@gmail.com>
2024-09-13 19:14:06 +0600

    finished the vertical portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: ['*'],
   theme: {
     extend: {
-      fontFamily :{
-        poppins: ["Poppins", "sans-seri"]
+      fontFamily: {
+        poppins: ['Poppins', 'sans - serif'],
       },
       colors: {
-        "bg-color": "#091420",
-        "bg-dark-color": "#0c1a28",
-        "primary-color": "#4fe98c",
+        'bg-color': '#091420',
+        'bg-dark-color': '#0c1a28',
+        'primary-color': '#4fe98c',
       },
       width: {
-        'custom-image-width' : '200px'
-      }
+        'custom-image-width': '200px',
+      },
     },
   },
   plugins: [],
